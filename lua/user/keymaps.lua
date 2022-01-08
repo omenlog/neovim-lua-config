0cb2b7d5ed90b8b8cccf35c656d98f34ab5cfd3e
o.lopez <o.lopez@draftkings.com>
2022-01-08 15:15:22 +0200

    add comment to show different modes used in keymapings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,6 +5,14 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
