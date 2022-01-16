98b84f80046831c170bb691872d987bd3227d683
o.lopez <o.lopez@draftkings.com>
2022-01-16 21:48:29 +0200

    set tab behaviour

--- lua/user/config.lua
+++ lua/user/config.lua
@@ -1,14 +1,18 @@
 local set = vim.opt
+local global = vim.g
 
 set.number = true
 set.termguicolors = true
 set.swapfile = false 
 set.backup = false
-set.tabstop = 2
+set.tabstop = 4
 set.smartindent = true
-set.shiftwidth = 2
+set.shiftwidth = 4
 set.diffopt="vertical"
 set.cmdheight = 2
+set.expandtab = true
+set.smarttab = true
 set.foldmethod="indent"
+
 -- Setting AutoSave when the Neovim lost focus
 vim.api.nvim_command('autocmd FocusLost * silent update')
