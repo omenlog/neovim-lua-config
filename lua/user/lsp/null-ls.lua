32cb1435c3a895354b194f45e0e4a7d24dd52a9d
o.lopez <o.lopez@draftkings.com>
2022-05-08 16:00:43 +0300

    (chore) simplify null-ls configuration
    - using eslint_d for code formatting
    - using tsc for code diagnostics

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -10,31 +10,16 @@ null_ls.setup({
 	debug = false,
 	sources = {
         formatting.stylua,
-        --
-        -- Eslint formatting
-		formatting.eslint_d.with({
-			filetypes = {"javascript", "javascriptreact"},
-			extra_args = {"--fix"}
-		}),
-		formatting.eslint_d.with({
-			filetypes = {"typescript", "typescriptreact"},
-      extra_args = { "--no-eslintrc","--config", "ts.eslint.json" },
-			condition = function(utils)
-				return utils.root_has_file({"ts.eslint.json"})
-			end
-		}),
+		formatting.eslint_d,
         --
         -- Eslint diagnostics
 		diagnostics.eslint_d.with({
-			filetypes = {"javascript", "javascriptreact"}
-		}),
-		diagnostics.eslint_d.with({
-			filetypes = {"typescript", "typescriptreact"},
-            extra_args = { "--no-eslintrc","--config", "ts.eslint.json" },
-			condition = function(utils)
-				return utils.root_has_file({"ts.eslint.json"})
-			end
-		}),
+            filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+            extra_args = {"--config", ".eslintrc"}
+        }),
+        diagnostics.tsc.with({
+            prefer_local = "node_modules/.bin"
+        }),
 		diagnostics.stylelint.with({
             extra_args = {"--config" ,""},
             prefer_local = "node_modules/.bin"
