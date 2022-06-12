b5b4d324406180fa44a64709ddae053c74b0c6c4
o.lopez <o.lopez@draftkings.com>
2022-06-12 15:38:05 +0300

    (add) javascript snippets

--- lua/user/snippets/javascript.lua
+++ lua/user/snippets/javascript.lua
@@ -5,6 +5,8 @@ local M = {}
 
 local s = ls.snippet
 local i = ls.insert_node
+local c = ls.choice_node
+local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 local imports_snippets = {
@@ -22,10 +24,51 @@ local console_snippets = {
     s("ce", fmt("console.error('{}');", {i(1)})),
 }
 
+local function_snippets = {
+    s("fn", 
+        fmt("{}",{ c(1, {
+            fmt(
+            [[
+                function {}({}) {{
+                    {}
+                }}
+            ]], {i(1), i(2), i(3)}),
+            fmt(
+            [[
+                const {} = function ({}) {{
+                    {}
+                }}
+            ]], {i(1), i(2), i(3)}),
+            fmt(
+            [[
+                function ({}) {{
+                    {}
+                }}
+            ]], {i(1), i(2)})
+        })})
+    ),
+    s("arf", 
+        fmt("{}",{ c(1, {
+            fmt(
+            [[
+                ({}) => {}
+            ]], {i(1), i(2)}),
+            fmt(
+            [[
+                const {} = ({}) => {{
+                    {}
+                }}
+            ]], {i(1), i(2), i(3)}),
+        })})
+    ),
+
+}
+
 local snippets = utils.merge(
     imports_snippets, 
     exports_snippets,
-    console_snippets
+    console_snippets,
+    function_snippets
 )
  
 M.setup = function()
