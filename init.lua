11883bfc8b0fd8e192b294a1dd932406facef6bb
o.lopez <omar.enrique1211@gmail.com>
2026-03-20 20:59:17 +0100

    config folding

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 -- Basic Settings
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+
+-- Folding
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
 
 -- Clipboard
 vim.opt.clipboard = "unnamed"
