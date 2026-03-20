f52ff514557b7a87399ccba21719721f4cb483a9
o.lopez <omar.enrique1211@gmail.com>
2026-03-20 20:59:12 +0100

    install more themes

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -23,25 +23,25 @@ return {
 		name = "kanagawa",
 		priority = 1000,
 	},
-		{
-			"catppuccin/nvim",
-			name = "catppuccin",
-			priority = 1000,
-			config = function()
-				require("catppuccin").setup({
-					integrations = {
-						treesitter = true,
-					},
-					custom_highlights = function(colors)
-						return {
-							["@tag"] = { fg = colors.blue },
-							["@tag.attribute"] = { fg = colors.teal },
-							["@tag.delimiter"] = { fg = colors.sky },
-						}
-					end,
-				})
-			end,
-		},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					treesitter = true,
+				},
+				custom_highlights = function(colors)
+					return {
+						["@tag"] = { fg = colors.blue },
+						["@tag.attribute"] = { fg = colors.teal },
+						["@tag.delimiter"] = { fg = colors.sky },
+					}
+				end,
+			})
+		end,
+	},
 	{
 		"zaldih/themery.nvim",
 		lazy = false,
