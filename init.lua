f219eca1a51bcc5a9d7a34152bf21d5031893524
omenlog <omar.enrique1211@gmail.com>
2026-03-24 14:09:12 +0100

    allow nvim to auto reload content after external tools code change

--- init.lua
+++ init.lua
@@ -8,6 +8,24 @@ vim.opt.shiftwidth = 2
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
 
+-- set autoread
+vim.opt.autoread = true
+
+vim.opt.updatetime = 200
+
+local auto_reload_external = vim.api.nvim_create_augroup("auto_reload_external", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained", "VimResume" }, {
+	group = auto_reload_external,
+	callback = function()
+		if vim.bo.buftype == "" and vim.fn.mode() ~= "c" then
+			vim.cmd("silent! checktime")
+		end
+	end,
+})
+
+
+
 -- Folding
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
