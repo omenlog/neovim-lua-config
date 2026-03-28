0d728b5d6f382576b2d24632fb02d3d28ca6fa9b
omenlog <omar.enrique1211@gmail.com>
2026-03-28 21:42:44 +0100

    adjust gitsigns symbols

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,9 +5,9 @@ return {
 		config = function()
 			require('gitsigns').setup {
 				signs                        = {
-					add          = { text = '┃' },
-					change       = { text = '┃' },
-					delete       = { text = '_' },
+					add          = { text = '+' },
+					change       = { text = '~' },
+					delete       = { text = '-' },
 					topdelete    = { text = '‾' },
 					changedelete = { text = '~' },
 					untracked    = { text = '┆' },
