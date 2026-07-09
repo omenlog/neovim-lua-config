f6d698242423b934278ccc72b40b1c3fe48c8d6c
omenlog <omar.enrique1211@gmail.com>
2026-07-09 09:36:51 +0200

    plugin: enable lsp to view hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,20 @@
 return {
-{
-    'nvim-telescope/telescope.nvim', version = '*',
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-}
+      'nvim-lua/plenary.nvim',
+      -- optional but recommended
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+      })
+    end,
+  },
 }
