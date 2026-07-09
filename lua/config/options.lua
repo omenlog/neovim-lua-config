fb828b6906f714c23ec44612c4c954607ea78338
omenlog <omar.enrique1211@gmail.com>
2026-07-09 11:10:40 +0200

    chore: disable unused providers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
+-- Disable unused providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- Basic Settings
 vim.opt.number = true
 vim.opt.relativenumber = true
