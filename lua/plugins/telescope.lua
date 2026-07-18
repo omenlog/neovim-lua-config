1bdc08069dd6676fa06e0d874acef8f6d4819bf0
omenlog <omar.enrique1211@gmail.com>
2026-07-18 22:04:45 +0200

    plugin: make telescope to ignore .git directory
    but add support for hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,21 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    version = '*',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- optional but recommended
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    config = function()
-      require('telescope').setup({
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-        },
-      })
-    end,
-  },
+	{
+		'nvim-telescope/telescope.nvim',
+		version = '*',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			-- optional but recommended
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		},
+		config = function()
+			require('telescope').setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+						find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' },
+					}
+				},
+			})
+		end,
+	},
 }
