b38cf5a5fa79f7ede388c3d037163418279cd035
omenlog <omar.enrique1211@gmail.com>
2026-07-18 21:53:58 +0200

    plugins: go back to lazygit integration instead of neogit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,7 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.lazygit_floating_window_scaling_factor = 1.0
 
 -- Basic Settings
 vim.opt.number = true
