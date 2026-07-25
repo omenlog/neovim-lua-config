250882f74edaa7b8d5df6f1d984e4f5fdeb6c442
omenlog <omar.enrique1211@gmail.com>
2026-07-25 12:35:54 +0200

    keymap: use vim.ui in cp keymap

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,39 +1,31 @@
 local M = {}
 
 function M.copy_ref(options)
-	local path   = vim.fn.expand("%:.")
+	local path = vim.fn.expand("%:.")
 	local result = path
 
 	if options.visual == true then
-		-- v is the line where the visual mode started this is the anchor
 		local start_line = vim.fn.line("v")
-
-		-- this is the line where the cursor is at the moment
 		local end_line = vim.fn.line(".")
 
-		-- this means the selection was madem upward invert the numbers
 		if start_line > end_line then
 			start_line, end_line = end_line, start_line
 		end
 
-		-- now append the range to the result
 		result = path .. ":" .. start_line .. ":" .. end_line
-
-		-- exit visual mode
 		vim.api.nvim_feedkeys(vim.keycode("<Esc>"), "nx", false)
 	end
 
-	local note = vim.fn.input("Prompt (optional): ")
-	if note ~= "" then
-		-- append note to the result using an space as separator
-		result = result .. " " .. note
-	end
+	vim.ui.input({ prompt = "Prompt (optional): " }, function(note)
+		note = note or ""
 
-	-- adding result to the system clipboard which is + register
-	vim.fn.setreg('+', result)
+		if note ~= "" then
+			result = result .. " " .. note
+		end
 
-	-- showing a message that the text was added to the clipboard
-	vim.notify("Copied: " .. result)
+		vim.fn.setreg("+", result)
+		vim.notify("Copied: " .. result)
+	end)
 end
 
 return M
