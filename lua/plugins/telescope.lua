2e365e3cf5957a7f77f0b4d67dc02d88a783f4f3
omenlog <omar.enrique1211@gmail.com>
2026-08-02 14:34:49 +0200

    plugin: ignore node_modules from telescope searchs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
 				pickers = {
 					find_files = {
 						hidden = true,
-						find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' },
+						find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git', '--exclude', 'node_modules' },
 					}
 				},
 			})
