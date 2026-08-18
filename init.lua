f590f02ec70ec48b808ab54974ab3b26a1b5b1f4
omenlog <omar.enrique1211@gmail.com>
2026-08-18 10:54:57 +0200

    chore: define local leader before lazy config

--- init.lua
+++ init.lua
@@ -1,4 +1,13 @@
 require("config.options")
+
+-- Define leaders before lazy.nvim loads plugin mappings.
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set({ "n", "v", "o" }, "<Space>", "<Nop>", { silent = true })
+
+-- loading lazy plugin manager
+require("config.lazy")
+
 require("config.keymaps")
 
 local auto_reload_external = vim.api.nvim_create_augroup("auto_reload_external", { clear = true })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,11 +13,6 @@ local keymap = vim.api.nvim_set_keymap
 --   command_mode = "c",
 --
 
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 -- Save keymaps
 keymap('i', "kk", '<ESC>:w<CR>', opts);
 
